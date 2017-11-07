8b072ef4cd8345cab73b8c7d7c6e38f6b3dfc37a
jbh <bainzo@users.noreply.github.com>
2017-11-07 15:27:41 +0000

    Fix unit test by correcting email subject string

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -19,7 +19,7 @@ class SendLoginEmailViewTest(TestCase):
         self.assertTrue(mock_send_mail.called)
         (subject, body, from_email, to_list), kwargs = \
                 mock_send_mail.call_args
-        self.assertEqual(subject, 'Your login link for superlists')
+        self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlists')
         self.assertEqual(to_list, ['edith@example.com'])
 
