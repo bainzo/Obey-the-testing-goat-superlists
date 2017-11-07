376d49f931e2261fb774971c56d217055809408a
jbh <bainzo@users.noreply.github.com>
2017-11-07 15:24:00 +0000

    Add message for successfully sending login email

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 def send_login_email(request):
     email = request.POST['email']
     send_mail(
-        'Your login link for superlists',
-        'body text tbc',
+        'Your login link for Superlists',
+        'Use this link to log in',
         'noreply@superlists',
         [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
