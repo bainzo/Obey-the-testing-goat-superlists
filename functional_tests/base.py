adb64c2b810deb63d5137dffc80fdaa9da020394
jbh <bainzo@users.noreply.github.com>
2017-11-08 18:58:33 +0000

    Add POP3 handling to test login on staging server
    Note that when the test is run against the staging server it will need
    to be run with the following environment variables set in addition to
    `STAGING_SERVER`:
    
        TEST_POP3_HOST
        TEST_POP3_EMAIL
        TEST_POP3_PASSWORD
    
    On some servers, especially those which contain lots of emails, it is
    necessary to prepend 'recent:' to the email address.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -26,9 +26,12 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            self.pop3_host = os.environ.get('TEST_POP3_HOST')
+            self.pop3_email = os.environ.get('TEST_POP3_EMAIL')
+            self.pop3_password = os.environ.get('TEST_POP3_PASSWORD')
 
     def tearDown(self):
         self.browser.quit()
