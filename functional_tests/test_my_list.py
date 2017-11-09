b7f205ebacf1baa0ff2e74207bae2eee4aae49c7
jbh <bainzo@users.noreply.github.com>
2017-11-09 20:47:59 +0000

    Add name property to list model to finish my_lists

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -35,7 +35,6 @@ class MyListsTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_in(email)
 
-    @tag('wip')
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged in user
         self.create_pre_authenticated_session('edith@example.com')
