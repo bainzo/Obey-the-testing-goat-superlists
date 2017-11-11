79f5f2bc80c70b7f247e058f2c54a6879f57dd73
jbh <bainzo@users.noreply.github.com>
2017-11-11 19:21:38 +0000

    Attempt to test new_list saves owner using mocks
    The modified test (test_list_owner_is_saved_if_user_is_authenticated)
    does work, though it is not concise and probably indicated some
    refactoring is needed.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,7 +10,7 @@ from lists.forms import (
     ExistingListItemForm, ItemForm,
 )
 
-from unittest import skip
+from unittest.mock import patch
 
 class HomePageTest(TestCase):
 
@@ -170,9 +170,21 @@ class MyListsTest(TestCase):
         response = self.client.get('/lists/users/a@b.com/')
         self.assertEqual(response.context['owner'], correct_user)
 
-    def test_list_owner_is_saved_if_user_is_authenticated(self):
+    @patch('lists.views.List')
+    @patch('lists.views.ItemForm')
+    def test_list_owner_is_saved_if_user_is_authenticated(
+        self, MockItemForm, MockList
+    ):
         user = User.objects.create(email='a@b.com')
         self.client.force_login(user)
+        mock_list = MockList.return_value
+
+        def check_owner_assigned():
+            self.assertEqual(mock_list.owner, user)
+        mock_list.save.side_effect = check_owner_assigned
+
         self.client.post('/lists/new', data={'text': 'new item'})
-        list_ = List.objects.first()
-        self.assertEqual(list_.owner, user)
+
+        self.assertEqual(mock_list.owner, user)
+
+        mock_list.save.assert_called_once_with()
